8b0bea75bc63948db4b83efca75e9914e2fc7131
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-05 01:36:04 +0300

    Return the resolved promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([res1, res2]) => {
-      console.log(`${res1.body} ${res2.firstName} ${res2.lastName}`);
-    })
-    .catch(console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([res1, res2]) => console.log(`${res1.body} ${res2.firstName} ${res2.lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
