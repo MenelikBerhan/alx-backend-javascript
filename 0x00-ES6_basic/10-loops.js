9ad302ba3c95438e44ba987b9b6b4b6a15a3cba8
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-04 04:34:04 +0300

    Task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable  no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+  for (const value of array) {
+    array[i] = appendString + value;
+    i += 1;
+  }
+
+  return array;
+}
