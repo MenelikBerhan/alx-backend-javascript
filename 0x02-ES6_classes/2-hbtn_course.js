b2869d58e932c7b3bdd38061196c9f31e3b7272f
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-05 02:53:58 +0300

    Add check is students is an Array

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof (name) !== 'string') throw new TypeError('Name must be a string');
+
     if (typeof (length) !== 'number') throw new TypeError('Length must be a number');
+
+    if (!Array.isArray(students)) throw new TypeError('Students must be an array of strings');
+
     students.forEach((element) => {
-      if (typeof (element) !== 'string') throw new TypeError('students must be an array of strings');
+      if (typeof (element) !== 'string') throw new TypeError('Students must be an array of strings');
     });
 
     this._name = name;
