42a0a7af8a1cb2d51c5d1af6d5bfa29f33dbe05e
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-05 02:47:07 +0300

    Task 0-2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,25 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') throw new TypeError('Name must be a string');
+    if (typeof (length) !== 'number') throw new TypeError('Length must be a number');
+    students.forEach((element) => {
+      if (typeof (element) !== 'string') throw new TypeError('students must be an array of strings');
+    });
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() { return this._name; }
+
+  get length() { return this._length; }
+
+  get students() { return this._students; }
+
+  set name(value) { this._name = value; }
+
+  set length(value) { this._length = value; }
+
+  set students(value) { this._students = value; }
+}
