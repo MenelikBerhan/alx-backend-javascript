b9d0bdba22276fb1d8abf06dd8c0ea21c1bcd81f
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-05 04:57:44 +0300

    Add missing _ in var name

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -28,7 +28,7 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
@@ -43,4 +43,3 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 export default [student1, student2, student3, student4, student5];
-// console.log(listOfStudents);
