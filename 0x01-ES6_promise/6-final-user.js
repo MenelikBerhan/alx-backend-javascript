59a04ef121d67a436eface2d9430cb16c96b6def
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-07 14:52:14 +0300

    Declare return array inside then()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const resp = [];
-
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then(([res1, res2]) => {
+      const resp = [];
+
       resp.push({ status: res1.status, value: res1.status === 'fulfilled' ? res1.value : res1.reason });
       resp.push({ status: res2.status, value: res2.status === 'fulfilled' ? res2.value : res1.reason });
 
