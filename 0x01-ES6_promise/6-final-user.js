47cabfe33f67b122856a57712c2beb374326f411
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-07 04:36:08 +0300

    Return array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -8,19 +8,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   const resp = [];
 
-  // signUpUser(firstName, lastName)
-  //   .then((value) => { resp.push({ status: value.status, value: value.body }); },
-  //     (message) => { resp.push({ status: 400, value: message }); });
+  signUpUser(firstName, lastName)
+    .then((value) => { resp.push({ status: value.status, value: value.body }); },
+      (message) => { resp.push({ status: 400, value: message }); });
 
-  // uploadPhoto().then((value) => { resp.push({ status: 200, value }); },
-  //   (message) => { resp.push({ status: 400, value: message }); });
+  uploadPhoto().then((value) => { resp.push({ status: 200, value }); },
+    (message) => { resp.push({ status: 400, value: message }); });
 
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto()])
-    .then(([res1, res2]) => {
-      resp.push({ status: res1.status, value: res1.body });
-      resp.push({ status: 200, res2 });
-    })
-    .catch((reason) => { resp.push({ status: 400, value: reason }); });
+  // return Promise.all([signUpUser(firstName, lastName), uploadPhoto()])
+  //   .then(([res1, res2]) => {
+  //     resp.push({ status: res1.status, value: res1.body });
+  //     resp.push({ status: 200, res2 });
+  //   })
+  //   .catch((reason) => { resp.push({ status: 400, value: reason }); });
 
-  // return (resp);
+  return (resp);
 }
