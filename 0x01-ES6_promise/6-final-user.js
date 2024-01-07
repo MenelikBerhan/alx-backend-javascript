d99506fd97ad32b552f61e91097d805ad97e7343
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-07 04:32:20 +0300

    Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './utils';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // const signUp = signUpUser(firstName, lastName);
+  // const upload = uploadPhoto();
+
+  const resp = [];
+
+  // signUpUser(firstName, lastName)
+  //   .then((value) => { resp.push({ status: value.status, value: value.body }); },
+  //     (message) => { resp.push({ status: 400, value: message }); });
+
+  // uploadPhoto().then((value) => { resp.push({ status: 200, value }); },
+  //   (message) => { resp.push({ status: 400, value: message }); });
+
+  // return Promise.all([signUpUser(firstName, lastName), uploadPhoto()])
+  //   .then(([res1, res2]) => {
+  //     resp.push({ status: res1.status, value: res1.body });
+  //     resp.push({ status: 200, res2 });
+  //   })
+  //   .catch((reason) => { resp.push({ status: 400, value: reason }); });
+  return Promise.all([signUpUser(firstName, lastName)])
+    .then(([res1]) => {
+      resp.push({ status: res1.status, value: res1.body });
+    })
+    .catch((reason) => { resp.push({ status: 400, value: reason }); });
+  // return (resp);
+}
