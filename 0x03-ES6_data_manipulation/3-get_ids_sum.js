8a4239dd12cd798a404ef88ce787bc2cb09e1ac2
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-08 23:23:34 +0300

    Task 0 - 3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+/* a function that returns the sum of all the student ids. */
+export default function getStudentIdsSum(students) {
+  return students.reduce(
+    (sum, student) => sum + student.id, 0,
+  );
+}
