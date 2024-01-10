0daff045a597f05343de069a0dd0bebfeb793377
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-10 22:47:29 +0300

    Task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+type Teacher = {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
