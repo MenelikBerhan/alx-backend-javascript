9df7168b895f8bb0eec532043a8397c264e5ffc6
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-11 00:46:21 +0300

    Task 3 & 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,9 +4,40 @@ type Teacher = {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [propName: string]: any;
+  [propName: string]: unknown;
 }
 
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0] + lastName}`
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string { return 'Currently working'; }
+
+  displayName(): string { return this.firstName; }
+};
