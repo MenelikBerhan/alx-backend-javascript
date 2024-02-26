19e5f6469d76cc79364d3e8aa865973788713064
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-27 01:15:42 +0300

    Import readline separately

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
 // read inputs from CLI
-const readline = require('node:readline').createInterface({
+const readline = require('node:readline');
+
+const r1 = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+r1.question('Welcome to Holberton School, what is your name?\n', (name) => {
   process.stdout.write(`Your name is: ${name}\n`);
   if (!process.stdin.isTTY) process.stdout.write('This important software is now closing\n');
-  readline.close();
+  r1.close();
 });
