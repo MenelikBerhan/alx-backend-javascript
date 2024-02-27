69c2e27361b23c57dc3bc6f5e973cffe80063475
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-27 05:40:06 +0300

    Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,14 +6,16 @@ function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
+  const data = fs.readFileSync(path, 'utf-8');
+  const studentList = data.toString().split('\n').slice(1).filter((line) => line.trim() !== '');
   // fileter out first line (column names) and empty last line
-  const studentList = fs.readFileSync(path, { encoding: 'utf-8' }).split('\n')
-    .filter((value, index) => value.trim() !== '' && index !== 0);
+  // const studentList = fs.readFileSync(path, { encoding: 'utf-8' }).split('\n')
+  //   .filter((value, index) => value.trim() !== '' && index !== 0);
   // stores <field_name>:<students_list> as key value pair
   const fieldDict = {};
   // for each line add first name of student to array of the respective field
   studentList.forEach((element) => {
-    const elementList = element.split(',');
+    const elementList = element.toString().split(',');
     const firstName = elementList[0];
     const fieldName = elementList[3];
     // if no entry for fieldName before, assign an empty array to it
