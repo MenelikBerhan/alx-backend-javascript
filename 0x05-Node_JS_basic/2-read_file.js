b0b551f07d2b8a83a189d6307252f12154d45a57
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-27 05:26:11 +0300

    Test

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,30 @@
-// read file synchronously and print info
 const fs = require('fs');
 
-function countStudents(path) {
-  // check if file exists
-  if (!fs.existsSync(path)) {
+function countStudents(file) {
+  let students = 0;
+  const subjects = {};
+  try {
+    const data = fs.readFileSync(file, 'utf-8');
+    const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i];
+      const fields = line.toString().split(',');
+      const field = fields[3];
+      const firstName = fields[0];
+
+      if (!subjects[field]) {
+        subjects[field] = { count: 0, names: [] };
+      }
+      subjects[field].count += 1;
+      subjects[field].names.push(firstName);
+      students += 1;
+    }
+    console.log(`Number of students: ${students}`);
+    Object.keys(subjects).forEach((field) => {
+      console.log(`Number of students in ${field}: ${subjects[field].count}. List: ${subjects[field].names.join(', ')}`);
+    });
+  } catch (error) {
     throw new Error('Cannot load the database');
-  }
-  // fileter out first line (column names) and empty last line
-  const studentList = fs.readFileSync(path, { encoding: 'utf-8' }).trim().split('\n')
-    .filter((value, index) => value !== '' && index !== 0);
-  // stores <field_name>:<students_list> as key value pair
-  const fieldDict = {};
-  // for each line add first name of student to array of the respective field
-  studentList.forEach((element) => {
-    const elementList = element.split(',');
-    const firstName = elementList[0];
-    const fieldName = elementList[3];
-    // if no entry for fieldName before, assign an empty array to it
-    if (!Object.hasOwn(fieldDict, fieldName)) fieldDict[fieldName] = [];
-    fieldDict[fieldName].push(firstName); // add student to array of fields
-  });
-  console.log(`Number of students: ${studentList.length}`);
-  for (const field of Object.keys(fieldDict)) {
-    console.log(`Number of students in ${field}: ${fieldDict[field].length}. List: ${fieldDict[field].join(', ')}`);
   }
 }
 
