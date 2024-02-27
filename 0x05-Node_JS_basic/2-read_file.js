105bf420acb898edfbac4805adb24662cb2c1e65
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-27 04:42:48 +0300

    Task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+// read file synchronously and print info
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    // fileter out first line (column names) and empty last line
+    const studentList = fs.readFileSync(path, { encoding: 'utf-8' }).split('\n')
+      .filter((value, index) => value !== '' && index !== 0);
+    // stores <field_name>:<students_list> as key value pair
+    const fieldDict = {};
+    // for each line add first name of student to array of the respective field
+    studentList.forEach((element) => {
+      const elementList = element.split(',');
+      const firstName = elementList[0];
+      const fieldName = elementList[3];
+      // if no entry for fieldName before, assign an empty array to it
+      if (!Object.hasOwn(fieldDict, fieldName)) fieldDict[fieldName] = [];
+      fieldDict[fieldName].push(firstName); // add student to array of fields
+    });
+    console.log(`Number of students: ${studentList.length}`);
+    for (const field of Object.keys(fieldDict)) {
+      console.log(`Number of students in ${field}: ${fieldDict[field].length}. List: ${fieldDict[field].join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
