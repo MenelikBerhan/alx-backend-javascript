bb2544aa3a00dfc9e66137d24dabd303e8fe1c82
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-29 02:42:59 +0300

    Task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,23 @@
+// test for sendPaymentRequestToApi
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  const logSpy = sinon.spy(console, 'log'); // spy tracks log method
+  // beforeEach(function beforeTestHook() {
+  //   logSpy.resetHistory();
+  // });
+  afterEach(function afterTestHook() {
+    logSpy.resetHistory();
+  });
+
+  it('Logs correct information to console.', function () {
+    sendPaymentRequestToApi(100, 20);
+    expect(logSpy.calledOnceWithExactly('The total is: 120')).to.equal(true);
+  });
+  it('Logs correct information to console.', function () {
+    sendPaymentRequestToApi(10, 10);
+    expect(logSpy.calledOnceWithExactly('The total is: 20')).to.equal(true);
+  });
+});
